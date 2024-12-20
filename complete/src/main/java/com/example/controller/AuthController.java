4daa88381dc2c9073ee90741375b8319bb365572
Jakub Piskorz <jakub.piskorz@gazeta.pl>
2024-12-21 00:36:12 +0100

    feat: enabled CORS

--- complete/src/main/java/com/example/controller/AuthController.java
+++ complete/src/main/java/com/example/controller/AuthController.java
@@ -8,10 +8,7 @@ import com.example.service.AuthService;
 import com.example.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
