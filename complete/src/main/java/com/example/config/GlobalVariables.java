6ba2778c67b8f8183615707bad8a4c4c81059ebe
Jakub Piskorz <jakub.piskorz@gazeta.pl>
2025-01-13 01:12:25 +0100

    fix: sanitized database credentials

--- complete/src/main/java/com/example/config/GlobalVariables.java
+++ complete/src/main/java/com/example/config/GlobalVariables.java
@@ -3,5 +3,11 @@ package com.example.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "var")
-public record GlobalVariables(String firstName, String lastName, String secretKey, Long expirationMs) {
+public record GlobalVariables(
+        String firstName,
+        String lastName,
+        String secretKey,
+        Long expirationMs,
+        String dbUsername,
+        String dbPassword) {
 }
