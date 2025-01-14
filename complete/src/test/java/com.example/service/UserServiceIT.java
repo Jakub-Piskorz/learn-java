0b2275332b36bb8ff143b6ef0053819156d59456
Jakub Piskorz <jakub.piskorz@gazeta.pl>
2025-01-14 01:44:46 +0100

    feat: First integration test

---
+++ complete/src/test/java/com.example/service/UserServiceIT.java
@@ -0,0 +1,28 @@
+package com.example.service;
+
+import com.example.model.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+// Integration test for {@link GameService}
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+public class UserServiceIT {
+    private final UserService userTest;
+
+    @Autowired
+    public UserServiceIT(UserService userTest) {
+        this.userTest = userTest;
+    }
+
+    @Test
+    public void testThatUserServiceFindsQbek() {
+        final User result = userTest.findByUsername("qbek");
+        assertThat(result.getLastName()).isEqualTo("Qbeczek");
+    }
+}
