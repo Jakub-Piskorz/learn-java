8943f4275647c78f5268dfc942ae33554e7e46a5
Jakub Piskorz <jakub.piskorz@gazeta.pl>
2025-01-15 16:11:21 +0100

    chore: removed unnecessary code

--- complete/src/main/java/com/example/model/UserLogin.java
+++ complete/src/main/java/com/example/model/UserLogin.java
@@ -4,14 +4,11 @@ package com.example.model;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @Getter
 @Setter
 @RequiredArgsConstructor
 public class UserLogin {
-    @Autowired
     private final String username;
-    @Autowired
     private final String password;
 }
