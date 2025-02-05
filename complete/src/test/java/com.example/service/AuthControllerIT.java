5b9a0b6b68d729a798e617fa8f062f2d434947a7
Jakub Piskorz <jakub.piskorz@gazeta.pl>
2025-02-05 23:01:28 +0100

    fix: made userAuthIT finally work

--- complete/src/test/java/com.example/service/AuthControllerIT.java
+++ complete/src/test/java/com.example/service/AuthControllerIT.java
@@ -2,6 +2,7 @@ package com.example.service;
 
 import com.example.model.User;
 import com.example.model.UserLogin;
+import com.example.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -25,9 +26,24 @@ public class AuthControllerIT {
     @ServiceConnection
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:latest");
 
+    @Autowired
+    UserRepository userRepository;
+
+
     @Test
-    public void testRegister() throws Exception {
-        UserLogin userLogin = new UserLogin("qbek", "gay");
+    public void userAuthIT() throws Exception {
+        User user = restTemplate.postForObject(
+                "/auth/register",
+                new User("qbek", "test@test.com", "testFirstname", "testLastname", "secret"),
+                User.class);
+        assertThat(user).isNotNull();
+        assertThat(user.getId()).isNotNull();
+        assertThat(user.getPassword()).isNotNull();
+        assertThat(user.getEmail()).isNotNull();
+        assertThat(user.getFirstName()).isNotNull();
+        assertThat(user.getLastName()).isNotNull();
+
+        UserLogin userLogin = new UserLogin("qbek", "secret");
         String jwtToken = restTemplate.postForObject("/auth/login", userLogin, String.class);
         assertThat(jwtToken.length()).isGreaterThan(0);
         ;
