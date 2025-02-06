955402d82e83a41271ca0673c06ec6f2b0942359
Jakub Piskorz <jakub.piskorz@gazeta.pl>
2025-02-06 20:57:27 +0100

    feat: added env variables to integration tests

--- complete/src/test/java/com.example/service/AuthControllerIT.java
+++ complete/src/test/java/com.example/service/AuthControllerIT.java
@@ -1,8 +1,8 @@
 package com.example.service;
 
+import com.example.config.GlobalVariables;
 import com.example.model.User;
 import com.example.model.UserLogin;
-import com.example.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,19 +22,19 @@ public class AuthControllerIT {
     @Autowired
     TestRestTemplate restTemplate;
 
+    @Autowired
+    private GlobalVariables env;
+
     @Container
     @ServiceConnection
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:latest");
 
-    @Autowired
-    UserRepository userRepository;
-
 
     @Test
-    public void userAuthIT() throws Exception {
+    public void userAuthIT() {
         User user = restTemplate.postForObject(
                 "/auth/register",
-                new User("qbek", "test@test.com", "testFirstname", "testLastname", "secret"),
+                new User(env.ffUsername(), "test@test.com", "testFirstname", "testLastname", env.ffPassword()),
                 User.class);
         assertThat(user).isNotNull();
         assertThat(user.getId()).isNotNull();
@@ -43,10 +43,9 @@ public class AuthControllerIT {
         assertThat(user.getFirstName()).isNotNull();
         assertThat(user.getLastName()).isNotNull();
 
-        UserLogin userLogin = new UserLogin("qbek", "secret");
+        UserLogin userLogin = new UserLogin(env.ffUsername(), env.ffPassword());
         String jwtToken = restTemplate.postForObject("/auth/login", userLogin, String.class);
         assertThat(jwtToken.length()).isGreaterThan(0);
-        ;
     }
 
 }
