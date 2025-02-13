da91fd5d24a2faae3cf65ee8d0c70a6801e8d804
Jakub Piskorz <jakub.piskorz@gazeta.pl>
2025-02-13 03:10:30 +0100

    feat: Added SSL and moved the server to HTTPS

--- complete/src/main/java/com/example/config/SecurityConfig.java
+++ complete/src/main/java/com/example/config/SecurityConfig.java
@@ -36,6 +36,9 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+//              Requires HTTPS
+                .requiresChannel(channel ->
+                        channel.anyRequest().requiresSecure())
                 .authorizeHttpRequests((a) -> a
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/").permitAll()
